73a10bbe82638887e2c007422c26754390297f03
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-02-06 20:41:02 -0800

    finish arithmetic module and relevant tests

--- __test__/basics.test.js
+++ __test__/basics.test.js
@@ -2,7 +2,7 @@
 
 const faker = require('faker');
 const greet = require('../lib/greet');
-const arithmetic = require('../arithmetic');
+const arithmetic = require('../lib/arithmetic');
 
 describe('#lib/greet.js', () => {
   test('These are the tests for the greet function', () => {
@@ -31,8 +31,9 @@ describe('#arithmetic.js', () => {
     //    ARRANGE
     const numA = faker.random.number();
     const numB = faker.random.number();
-    // const aWord = faker.lorem.word();
-    // const aBoolean = false;
+    const numC = faker.random.number();
+    const aWord = faker.lorem.word();
+
 
     //    ACT
     //    Andrew - no ACT for this
@@ -43,5 +44,15 @@ describe('#arithmetic.js', () => {
     expect(arithmetic.multiply(numA, numB)).toEqual(numA * numB);
     expect(arithmetic.divide(numA, numB)).toEqual(numA / numB);
 
+    expect(arithmetic.add(numA, aWord)).toBeNull();
+    expect(arithmetic.subtract(aWord, numB)).toBeNull();
+    expect(arithmetic.multiply(numA, aWord)).toBeNull();
+    expect(arithmetic.divide(aWord, numB)).toBeNull();
+
+    expect(arithmetic.add([numA, numB, numC])).toEqual(numA + numB + numC);
+    expect(arithmetic.subtract([numA, numB, numC])).toEqual(numA - numB - numC);
+    expect(arithmetic.multiply([numA, numB, numC])).toEqual(numA * numB * numC);
+
+    expect(arithmetic.divide(numA, 0)).toBeNull();
   });
 });
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const arithmetic = require('./arithmetic');
+const arithmetic = require('./lib/arithmetic');
 const greet = require('./lib/greet');
 
 console.log(greet('Andrew'));
