81c5b4059bfa7dfca94d91a8b5c20ecc70f50300
Andrew Davis <andrew.bradley.davis@gmail.com>
2019-02-06 16:59:23 -0800

    cleanup test

--- __test__/basics.test.js
+++ __test__/basics.test.js
@@ -10,26 +10,17 @@ describe('#lib/greet.js', () => {
         const aNumber = 5;
         const aBoolean = true;
         const aName = faker.name.firstName();
-
+        const world = 'world';
 
     //    ACT
-    //    Andrew - no ACT for this demo - will have plenty later\
+    //    Andrew - no ACT for this
 
     //    ASSERT
         expect(greet(aNumber)).toBeNull();
         expect(greet(aBoolean)).toBeNull();
         expect(greet(aName)).toEqual(`hello ${aName}`);
+        expect(greet(world)).toEqual('hello world');
 
     });
 
-
-    // test('#greet.sayHi should say Hi and ask about Khaleesi', () => {
-    // //    ARRANGE - not now
-    //
-    // //    ACT
-    //     const returnValue = greet.sayHi('Banana');
-    //
-    // //    ASSERT
-    //     expect(returnValue).toEqual('Hello Banana. Have you met my dog Khaleesi?');
-    // })
 });
